bf97e073a4bf988fb32823c3bf08a071d7def7c8
lee-ann36 <readleeann@gmail.com>
2026-05-28 10:34:32 +0200

    j j kjn
    jkjn

--- src/AdminDashboard.php
+++ src/AdminDashboard.php
@@ -6,7 +6,7 @@ $username = "root";
 $password = "password";
 
 try {
-    $conn = new mysqli($servername, $username, $password, "tools4ever");
+    $conn = new mysqli($servername, $username, $password, "mydb");
 } catch (mysqli_sql_exception) {
     echo "Could not connect";
     exit();
@@ -20,9 +20,30 @@ try {
 <head>
     <meta charset="UTF-8">
     <title>Beheerderspaneel</title>
-    <link rel="stylesheet" href="#">
+    <link rel="stylesheet" href="/css/AD.css">
 </head>
 <body>
+<div class= "navbar">
+    <a href="#" class="nav-item active">
+    <i class="icon">⌂</i>
+    <span>Dashboard</span>
+  </a>
 
+  <a href="#" class="nav-item">
+    <i class="icon">👥</i>
+    <span>Instructeurs</span>
+  </a>
+
+  <a href="#" class="nav-item">
+    <i class="icon">👤</i>
+    <span>Leerlingen</span>
+  </a>
+
+  <a href="#" class="nav-item">
+    <i class="icon">🚗</i>
+    <span>Wagenpark</span>
+  </a>
+
+</div>
 </body>
 </html>
